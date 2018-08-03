e064184c56069a055d5181bbc0ab027ab0f48a65
pee-que-elle <pql@cock.li>
2018-08-03 17:25:50 +0200

    recompiled protos to fix path problem

--- compile_protos.sh
+++ compile_protos.sh
@@ -1,10 +1,2 @@
-cd proto_definitions/game/PSXAPI.Request
-protoc --proto_path=../../ --proto_path="./" --python_out=../../../protos/game/request *.proto
-cd ../PSXAPI.Response
-protoc --proto_path=../../ --proto_path="./" --python_out=../../../protos/game/response/ *.proto
-cd ../PSXAPI.Response.Payload
-protoc --proto_path=../../ --proto_path="./" --python_out=../../../protos/game/response/payload *.proto
-cd ../../maps/MAPAPI.Request
-protoc --proto_path=../../ --proto_path="./" --python_out=../../../protos/maps/request/ *.proto
-cd ../MAPAPI.Response
-protoc --proto_path=../../ --proto_path="./" --python_out=../../../protos/maps/response *.proto
+cd ./proto_definitions 
+protoc --proto_path="./" --python_out=/mnt/d/code/p1_async_proxy/protos/ `find ./ -type f -name "*.proto"`
